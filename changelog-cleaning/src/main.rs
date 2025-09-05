6ee1fa9a1ce18f85f1e74d8cc2b030385bfb92d3
Gemma Tipper <gemmat@metalbear.co>
2025-09-05 15:59:28 +0100

    Add rust script dependency

--- changelog-cleaning/src/main.rs
+++ changelog-cleaning/src/main.rs
@@ -1,3 +1,15 @@
+#!/usr/bin/env rust-script
+//! This is a script that edits the changelog to be suitable for the website docs. It performs the following changes:
+//! 1. Remove links from main headings, which link to the (private) operator repo
+//! 2. Remove sections that start "### Internal"
+//! 3. Fix empty sections, replace with "No significant changes"
+//! 4. Remove links to GitHub issues
+//!
+//! ```cargo
+//! [dependencies]
+//! regex = "1.11"
+//! ```
+
 use std::io::{self, Read};
 use regex::Regex;
 
@@ -14,7 +26,7 @@ fn main() {
     let internal_regex = Regex::new(r"### Internal[\s\S]*?(?=##)").unwrap();
     let file_contents = internal_regex.replace_all(&file_contents, "");
 
-    // Fix empty sections, replace with "No significant changes", run twice to deal with overlaps
+    // Fix empty sections, replace with "No significant changes"
     let empty_section_regex = Regex::new(r"(## .+)(\n\n\n)(?=## )").unwrap();
     let file_contents = empty_section_regex.replace_all(&file_contents, "$1\n\nNo significant changes.$2");
 
