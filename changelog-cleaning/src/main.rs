2ba06f93c0191a507f2d3806529edb86423e2e38
Gemma Tipper <gemmat@metalbear.co>
2025-09-05 15:10:15 +0100

    Create rust script to run regex replacements on changelog file

--- changelog-cleaning/src/main.rs
+++ changelog-cleaning/src/main.rs
@@ -1,6 +1,29 @@
-fn main() {
-    println!("yayayay");
+use std::io::{self, Read};
+use regex::Regex;
 
+fn main() {
+    // Read file contents from stdin
+    let mut file_contents = String::new();
+    io::stdin().read_to_string(&mut file_contents).expect("Failed to read from stdin");
+
+    // Remove links from headings
+    let heading_regex = Regex::new(r"## \[(.+)\]\(.+\)").unwrap();
+    let file_contents = heading_regex.replace_all(&file_contents, "## $1");
+
+    // Remove sections that start "### Internal"
+    let internal_regex = Regex::new(r"### Internal[\s\S]*?(?=##)").unwrap();
+    let file_contents = internal_regex.replace_all(&file_contents, "");
+
+    // Fix empty sections, replace with "No significant changes", run twice to deal with overlaps
+    let empty_section_regex = Regex::new(r"(## .+)(\n\n\n)(?=## )").unwrap();
+    let file_contents = empty_section_regex.replace_all(&file_contents, "$1\n\nNo significant changes.$2");
+
+    // remove issue links
+    let empty_section_regex = Regex::new(r"\s*\[#\d*\]\(.*issues.*\)").unwrap();
+    let file_contents = empty_section_regex.replace_all(&file_contents, "");
+
+    // Print the file contents
+    println!("{}", file_contents);
 }
 
 
