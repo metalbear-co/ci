1623fd9fed03440bdf97987af925eeed303f81d5
Gemma Tipper <gemmat@metalbear.co>
2025-09-09 11:01:58 +0100

    Insert file preamble to output

--- changelog-cleaning/src/main.rs
+++ changelog-cleaning/src/main.rs
@@ -4,14 +4,17 @@
 //! 2. Remove sections that start "### Internal"
 //! 3. Fix empty sections, replace with "No significant changes"
 //! 4. Remove links to GitHub issues
+//! 5. Insert preamble
 //!
 //! ```cargo
 //! [dependencies]
-//! regex = "1.11"
+//! fancy-regex = "0.16"
+//! chrono = { version = "0.4.42", features = ["now"] }
 //! ```
 
 use std::io::{self, Read};
-use regex::Regex;
+use fancy_regex::Regex;
+use chrono::Utc;
 
 fn main() {
     // Read file contents from stdin
@@ -30,12 +33,31 @@ fn main() {
     let empty_section_regex = Regex::new(r"(## .+)(\n\n\n)(?=## )").unwrap();
     let file_contents = empty_section_regex.replace_all(&file_contents, "$1\n\nNo significant changes.$2");
 
-    // remove issue links
+    // Remove issue links
     let empty_section_regex = Regex::new(r"\s*\[#\d*\]\(.*issues.*\)").unwrap();
     let file_contents = empty_section_regex.replace_all(&file_contents, "");
+
+    // Insert preamble
+    let file_contents = format!("{}{file_contents}", preamble_with_date().as_str());
 
     // Print the file contents
     println!("{}", file_contents);
 }
 
-
+pub fn preamble_with_date() -> String {
+    format!(
+        "---\n\
+        title: Operator Changelog\n\
+        date: 2023-08-15T00:00:00.000Z\n\
+        lastmod: {}T00:00:00.000Z\n\
+        draft: false\n\
+        images: []\n\
+        weight: 100\n\
+        toc: true\n\
+        tags:\n  - team\n  - enterprise\n\
+        description: >-\n  The release changelog for the mirrord operator.\n\
+        ---\n\n",
+        Utc::now().date_naive()
+    
+    )
+}
